e1c9ab445a8337f2024cb7f7f39cbf11ae23a477
MohamedKadi <gamesmohamed14@gmail.com>
2025-02-09 23:09:50 +0100

    WeatherApp

---
+++ WeatherApp/src/partials/Card.jsx
@@ -0,0 +1,55 @@
+import { useEffect, useState } from 'react';
+
+export default function Card({ city, country, isSet }) {
+  const [cordinates, setCordinates] = useState({ lat: '', lon: '' });
+  const [data, setData] = useState(null);
+
+  const apiKey = import.meta.env.VITE_WEATHER_API_KEY;
+
+  useEffect(() => {
+    const fetchCord = async () => {
+      const response = await fetch(
+        `http://api.openweathermap.org/geo/1.0/direct?q=${city},${country}&limit=1&appid=${apiKey}`
+      );
+      const cord = await response.json();
+      if (cord.length > 0) {
+        setCordinates({ lat: cord[0].lat, lon: cord[0].lon });
+      }
+    };
+    if (city && country) {
+      fetchCord();
+    }
+  }, [city, country]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      if (cordinates.lat !== '' && cordinates.lon !== '') {
+        const response = await fetch(
+          `https://api.openweathermap.org/data/2.5/weather?lat=${cordinates.lat}&lon=${cordinates.lon}&appid=${apiKey}`
+        );
+
+        const data = await response.json();
+
+        setData(data);
+      }
+    };
+    fetchData();
+  }, [cordinates]);
+  if (isSet) {
+    return (
+      <>
+        {data && (
+          <div key={data.weather[0].id}>
+            <h3>
+              {city}, {country}
+            </h3>
+            <p>{data.weather[0].main}</p>
+            <p>{data.weather[0].description}</p>
+            <span>Temperature {data.main.temp}F</span>
+          </div>
+        )}
+      </>
+    );
+  }
+  return;
+}
---
+++ WeatherApp/src/partials/Search.jsx
@@ -0,0 +1,27 @@
+import { useState } from 'react';
+import Card from './Card';
+
+export default function Search() {
+  const [city, setCity] = useState(null);
+  const [country, setCountry] = useState(null);
+  const [isSet, setIsSet] = useState(false);
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    setCity(event.target.city.value);
+    setCountry(event.target.country.value);
+    setIsSet(true);
+  }
+
+  return (
+    <>
+      <form onSubmit={handleSubmit}>
+        <input type="text" name="city" />
+        <br />
+        <input type="text" name="country" />
+        <button type="submit">Search</button>
+      </form>
+      {isSet && <Card city={city} country={country} isSet={isSet} />}
+    </>
+  );
+}
